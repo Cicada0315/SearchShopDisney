e7e901f4c2a26b06a2803c3b5e181015b9cf854b
jean <jin.ahn.1027@gmail.com>
2021-06-07 20:08:58 -0400

    added title label into MySearchGUI

--- MySearchGUI.java
+++ MySearchGUI.java
@@ -1,8 +1,10 @@
 import javax.swing.*; 
 import java.awt.*; 
+import java.awt.event.*;
 
 public class MySearchGUI extends JFrame{
     private Container c;
+    private JLabel title;
 
     public MySearchGUI(){
         setTitle("Jin's SearchEngine");
@@ -12,5 +14,11 @@ public class MySearchGUI extends JFrame{
         c.setBackground((new Color(204, 229, 255)));
         c.setLayout(null);
         setVisible(true);
+
+        title = new JLabel("SearchDisney"); 
+        title.setFont(new Font("Jokerman", Font.PLAIN, 30)); 
+        title.setSize(250, 40); 
+        title.setLocation(325, 100); 
+        c.add(title);
     }
 }
